5ab75db9a89e7c7747840ae9953a1ad5fd5e8f77
Luan Goncalves dos Santos <p22.luan@webmotors.local>
2022-09-14 13:03:00 -0300

    melhorias e ajustes para rodar no jenkins

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,23 +16,42 @@ Faker::Config.locale = 'pt-BR'
 AMBIENTE = YAML.load_file("#{File.dirname(__FILE__)}/config/environments.yml")
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/config/#{AMBIENTE['ambiente']}.yml")
 
-Selenium::WebDriver::Chrome::Service.driver_path = './chromedriver.exe'
-
-Capybara.register_driver :chrome do |app|
-  chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |options|
-    options.add_argument '--disable-gpu'
-    options.add_argument '--no-sandbox'
-    options.add_argument '--disable-site-isolation-trials'
-    options.add_argument '--start-maximized'
-    options.add_argument 'user-agent="AUTOMATION-TESTING"'
+if ENV['headless']
+  Selenium::WebDriver::Chrome::Service.driver_path = "#{Dir.pwd}/webdrivers/chromedriver"
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    chrome_headless_options = Selenium::WebDriver::Chrome::Options.new.tap do |options|
+      options.add_argument '--headless'
+      options.add_argument '--disable-gpu'
+      options.add_argument '--no-sandbox'
+      options.add_argument '--incognito'
+      options.add_argument '--window-size=1280,1280'
+      options.add_argument '--disable-site-isolation-trials'
+      options.add_argument '--disable-dev-shm-usage'
+      options.add_argument 'user-agent="AUTOMATION-TESTING"'
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: chrome_headless_options)
   end
-  Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: chrome_options)
+  Capybara.default_driver = :selenium_chrome_headless
+else
+  Selenium::WebDriver::Chrome::Service.driver_path = './chromedriver.exe'
+  Capybara.register_driver :chrome do |app|
+    chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |options|
+      options.add_argument '--disable-gpu'
+      options.add_argument '--no-sandbox'
+      options.add_argument '--incognito'
+      options.add_argument '--window-size=1280,1280'
+      options.add_argument '--disable-site-isolation-trials'
+      options.add_argument '--start-maximized'
+      options.add_argument 'user-agent="AUTOMATION-TESTING"'
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: chrome_options)
+  end
+  Capybara.default_driver = :chrome
 end
 
 Capybara.configure do |config|
-  config.default_driver = :chrome
   config.app_host = CONFIG['url_webmotor']
-  config.default_max_wait_time = 20
+  config.default_max_wait_time = 30
   Capybara.page.driver.browser.manage.window.maximize
 end
 
